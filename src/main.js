20bb3c8770ef7d031f5fe422016f7a2b02fa1a88
Jakob Persons <jakobep24@gmail.com>
2021-06-06 10:10:08 -0400

    initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// variables declated using both ES% and ES6 syntax
+// ES5
+var first = "create npm project using npm init"
+
+// ES6
+const second = "check to make sure package.json was created"
+let third = "move on to next step: npm setup"
+
+// template literal introduced with ES6
+const instructions = `To start an npm project, first ${first}. Second, ${second}. Finally, ${third}.`
+
+console.log(instructions)
